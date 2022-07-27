c70e47f0fa1a0809004c211d9749b3b8792fa830
Marcel Britto <marcel.britto@zarpacrew.com.br>
2022-07-27 07:18:14 -0300

    Commit inicial.

---
+++ src/main/java/br/com/britto/SpringBootInitialApplication.java
@@ -0,0 +1,13 @@
+package br.com.britto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootInitialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootInitialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/britto/SpringBootInitialApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.britto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootInitialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
